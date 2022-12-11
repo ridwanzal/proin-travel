ebb81324b5fa9bc9518aceeb77679c995aeb9a6b
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2022-12-11 21:42:11 +0700

    Update cta for ads

--- web/resources/views/frontview/components/callout-one.blade.php
+++ web/resources/views/frontview/components/callout-one.blade.php
@@ -5,9 +5,8 @@
                 <div class="text-center">
                     <h4 class="text-white mb-4 text__medium-headline">Umroh dengan bimbingan dan ajaran<br />
                         sesuai sunnah Nabi Muhammad Salallahu alaihi’ wasallam</h4>
-                    <a href="https://api.whatsapp.com/send?phone=+{{ env('CUST_PHONE') }}&amp;text=Assalamualaikum Admin, Saya Ingin Tanya-Tanya Paket Umroh Boleh?"
+                    <a href="https://api.whatsapp.com/send?phone=+{{ env('CUST_PHONE') }}&amp;text={{ $source }}"
                         target="_blank" class="btn btn-md btn-success mb-3">Informasi Lengkap</a>
-
                 </div>
             </div>
         </div>
