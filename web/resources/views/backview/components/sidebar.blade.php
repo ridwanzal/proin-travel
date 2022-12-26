46dc94b076ad660f90ab687de929987b925d639b
M. Ridwan Zalbina <ridwanzal@fairtech.com.sg>
2022-12-26 16:14:30 +0700

    Update login implementation

--- web/resources/views/backview/components/sidebar.blade.php
+++ web/resources/views/backview/components/sidebar.blade.php
@@ -3,51 +3,6 @@
         <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
             <div class="position-sticky pt-1">
                 <ul class="nav flex-column">
-                    <li class="nav-item" style="background: #ccc;">
-                        <a href="{{ url('/') }}" class="nav-link text-black pt-3 pb-3" target="_blank">Go to landing
-                            page
-                            <i class="bi-arrow-right ms-5"></i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ url('dashboard') }}">
-                            <i class="dashi bi bi-envelope me-2"></i>
-                            Homepage
-                        </a>
-                    </li>
-                    <li class="nav-item d-block">
-                        <div class="accordion accordion-flush" id="accordionFlushExample">
-                            <div class="accordion-item">
-                                <h2 style="padding:0;" class="accordion-header nav-link" id="flush-headingOne">
-                                    <div class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapseOne" aria-expanded="false"
-                                        aria-controls="flush-collapseOne"
-                                        style="padding:0;background:#F8F9FA;font-size:14px;">
-                                        <span class="nav-link" href="{{ url('') }}">
-                                            <i class="dashi bi bi-grid me-2"></i>
-                                            Products
-                                        </span>
-                                        </button>
-                                </h2>
-                                <div id="flush-collapseOne" class="accordion-collapse collapse"
-                                    aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
-                                        <a class="nav-link" href="{{ url('backview/product-masthead') }}">
-                                            <i class="dashi bi bi-grid me-2"></i>
-                                            Product Masthead
-                                        </a>
-                                        <a class="nav-link" href="{{ url('backview/product-listing') }}">
-                                            <i class="dashi bi bi-grid me-2"></i>
-                                            Product Listing
-                                        </a>
-                                        <a class="nav-link" href="{{ url('backview/product-other') }}">
-                                            <i class="dashi bi bi-grid me-2"></i>
-                                            Other Product
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
                     <li class="nav-item d-none">
                         <div class="accordion accordion-flush" id="accordionFlushExample">
                             <div class="accordion-item">
@@ -102,6 +57,14 @@
                         <a class="nav-link" href="{{ url('backview/contact') }}">
                             <i class="dashi bi bi-files me-2"></i>
                             Contact Reports
+                        </a>
+                    </li>
+                </ul>
+                <ul class="nav flex-column mb-2">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('logout') }}">
+                            <i class="dashi bi bi-x-circle me-2"></i>
+                            Sign Out
                         </a>
                     </li>
                 </ul>
--- web/resources/views/backview/layout/app.blade.php
+++ web/resources/views/backview/layout/app.blade.php
@@ -40,6 +40,8 @@
 
 <link rel="preload" href="{{ asset('/css/style.css') }}" as="style" />
 <link rel=" stylesheet" href="{{ asset('/css/style.css') }}" />
+<link rel="preload" href="{{ asset('/css/dashboard.css') }}" as="style" />
+<link rel=" stylesheet" href="{{ asset('/css/dashboard.css') }}" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
