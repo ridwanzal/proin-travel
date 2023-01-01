a45e7111a88d909e093588811f5422a6ba09c25a
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2023-01-01 21:09:02 +0700

    Update unused data

--- web/app/Http/Controllers/PaketController.php
+++ web/app/Http/Controllers/PaketController.php
@@ -35,6 +35,47 @@ class PaketController extends Controller
             ->with('images', $images);
     }
 
+    public function update(Request $request)
+    {
+        $id = $request->id;
+        DB::table('paket')->where('id', $id)
+            ->update([
+                'title' => $request->title,
+                'content' => $request->content,
+                'price' => $request->price,
+                'cta' => $request->cta,
+                'cta_info' => $request->cta_info
+            ]);
+
+        return back()
+            ->with('success', 'You have successfully updated data.');
+    }
+
+    public function updateimage(Request $request)
+    {
+        $request->validate([
+            'images.*' => 'required|max:1000480',
+        ]);
+
+        $id = $request->id;
+        $images = [];
+        if ($request->images) {
+            foreach ($request->images as $key => $image) {
+                $imageName = time() . rand(1, 99) . '.' . $image->extension();
+                $image->move(public_path('images/uploads'), $imageName);
+                $images[]['name'] = $imageName;
+                DB::table('paket')->where('id', $id)
+                    ->update([
+                        'image' => $imageName,
+                    ]);
+            }
+        }
+
+        return back()
+            ->with('success', 'You have successfully upload image.')
+            ->with('images', $images);
+    }
+
     public function delete(Request $request)
     {
         $tableId = $request->id;
