45047a3f29da5566b12181e2207b7386a3f74f91
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2023-01-02 13:37:30 +0700

    Update anchor

--- web/resources/views/frontview/components/cta.blade.php
+++ web/resources/views/frontview/components/cta.blade.php
@@ -1,2 +1,7 @@
-<a href="https://api.whatsapp.com/send?phone=+{{ env('CUST_PHONE') }}&amp;text=Assalamualaikum Admin, Saya Ingin Tanya-Tanya Paket Umroh Boleh?"
-    target="_blank" class="btn btn-md btn-warning mb-3">Konsultasi sekarang</a>
+@if (sizeof($contacts) > 0)
+    <a href="https://api.whatsapp.com/send?phone=+{{ $contacts[0]->nomor_telepon }}&amp;text=Assalamualaikum Admin, Saya Ingin Tanya-Tanya Paket Umroh Boleh?"
+        target="_blank" class="btn btn-md btn-warning mb-3">Konsultasi sekarang</a>
+@else
+    <a href="https://api.whatsapp.com/send?phone=+{{ env('CUST_PHONE') }}&amp;text=Assalamualaikum Admin, Saya Ingin Tanya-Tanya Paket Umroh Boleh?"
+        target="_blank" class="btn btn-md btn-warning mb-3">Konsultasi sekarang</a>
+@endif
